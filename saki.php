b3e6b273da02b757e12916a16c760409425a1c0e
Lamda <takatim1574@icloud.com>
2018-03-12 10:20:18 +0900

    Add files via upload

--- saki.php
+++ saki.php
@@ -4,8 +4,19 @@ class saki extends \pocketmine\plugin\PluginBase{
     public function onCommand(\pocketmine\command\CommandSender $sender, \pocketmine\command\Command $cmd, strig $label, array $args): bool{
         switch ($cmd->getName()){
             case "saki":
-            \pocketmine\Server::getInstance()->broadcast("<".$sender."> 1145141919810");
+            $server = \pocketmine\Server::getInstance()
+            $server->broadcast("<★".$sender."★> 私さきだよJKだよ");
+            break;
+            case "lol":
+            $server = \pocketmine\Server::getInstance()
+            $server->broadcast("<★".$sender."★> lol");
+            break;
+            case "dane":
+            $server = \pocketmine\Server::getInstance()
+            $server->broadcast("<★".$sender."★> 私さきだよJKだよ");
+            break;
         }
+        return true;
     }
 }
 ?>
